22a12fdcffaf3c49ea803a1355f30136bf1814d2
tmsblgh <tmsblgh@me.com>
2018-04-07 15:31:34 +0200

    Add base application

---
+++ Running Assistant/Running Assistant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Running Assistant
+//
+//  Created by Balogh Tamás on 2018. 04. 07..
+//  Copyright © 2018. Balogh Tamás. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
